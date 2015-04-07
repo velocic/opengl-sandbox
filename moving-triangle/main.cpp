1a64c8a82ec7c1994de670c5c30f26906701ba7b
velocic <velocicdev@gmail.com>
2015-04-07 15:59:26 -0400

    Got very basic scaffold going, now to make it move

--- moving-triangle/main.cpp
+++ moving-triangle/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <shader.h>
+#include <programlinker.h>
 
 int main()
 {
@@ -28,7 +30,34 @@ int main()
 
 
     //Buffer Vertices (VBO, VAO)
+    GLuint vbo = 0;
+    GLuint vao = 0;
+    float vertexPositions[] = {
+        0.75f, 0.75f, 0.0f, 1.0f,
+        0.75f, -0.75f, 0.0f, 1.0f,
+        -0.75f, -0.75f, 0.0f, 1.0f
+    };
+
+
+    //load shaders, create program
+    Shader vert(GL_VERTEX_SHADER, "shaders/shader.vert");
+    Shader frag(GL_FRAGMENT_SHADER, "shaders/shader.frag");
+    std::vector<Shader> shaders = {
+        vert,
+        frag
+    };
+    ProgramLinker program(shaders);
+    program.link();
+    program.use();
     
+    //Load vertex data into a buffer pointed to by vbo
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
     //"game" loop
     SDL_Event e;
     bool userRequestedQuit = false;
@@ -40,14 +69,17 @@ int main()
             }
         }
         //handle drawing
-        
         glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         SDL_GL_SwapWindow(window);
     }
 
 
     //clean up
+    program.unuse();
+    glDisableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
     SDL_Quit();
